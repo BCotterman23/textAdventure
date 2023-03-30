3166c419a5d633281efb8c0252e56b410cace211
BCotterman23 <817363@mystma.org>
2023-03-30 15:08:21 -0500

    first commit

---
+++ text Adventure.py
@@ -0,0 +1,13 @@
+money = 0
+health = 3
+hunger = 1
+
+
+def stats():
+    global health
+    global hunger
+    print("Health: [" + str("*" * health) + str((10 - health)*"-") + "]")
+    print("Hunger: [" + str("*" * hunger) + str((10 - hunger)*"-") + "]")
+    print("Money: $" + str(money))
+
+stats()
