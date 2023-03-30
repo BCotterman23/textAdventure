4d182fbf62855db176145dba4d631b3f5b402d15
BCotterman23 <817363@mystma.org>
2023-03-30 15:13:46 -0500

    1

--- text Adventure.py
+++ text Adventure.py
@@ -6,6 +6,7 @@ hunger = 1
 def stats():
     global health
     global hunger
+    global money
     print("Health: [" + str("*" * health) + str((10 - health)*"-") + "]")
     print("Hunger: [" + str("*" * hunger) + str((10 - hunger)*"-") + "]")
     print("Money: $" + str(money))
