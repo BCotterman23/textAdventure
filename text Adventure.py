eac89d058835b6e1d38f7481c77ef18890628cb3
BCotterman23 <817363@mystma.org>
2023-03-30 22:45:04 -0500

    add highlights

--- text Adventure.py
+++ text Adventure.py
@@ -1,49 +1,48 @@
 #Make sure to first install colorama and termcolor to system via
 #pip install colorama
 #pip install termcolor
+
+#import neccesary Modules
 import time
 import sys
 from colorama import init
 from termcolor import colored, cprint
 init(autoreset= True)
 
-
+#define variables
 money = 0
 health = 5
 hunger = 0
 
+#stats dashboard
 def stats():
     global health
     global hunger
     global money
-    cprint(colored("Health", 'red') + ": [" + colored(str("*" * health), 'red') + str((10 - health)*"-") + "]", attrs=["bold"])
-    time.sleep(0.1)
-    cprint(colored("Hunger", 'black') + ": ["  + colored(str("*" * hunger), 'light_grey') + str((10 - hunger)*"-") + "]" , attrs=["bold"])
-    cprint("Money: $" + str(money), 'green', attrs=["bold"])
+    cprint(colored("Health", 'red', "on_black") + ": [" + colored(str("*" * health), 'red') + str((10 - health)*"-") + "]" + "     " , attrs=["bold"] , end="")
+    time.sleep(0.4)
+    cprint(colored("Hunger", 'white', "on_black") + ": ["  + colored(str("*" * hunger), 'light_grey') + str((10 - hunger)*"-") + "]" + "     " , attrs=["bold"], end="")
+    time.sleep(0.4)
+    cprint("Money: $" + str(money), 'green', "on_black" , attrs=["bold"])
+    time.sleep(0.4)
 
-stats()
+#typing animation
+def message(msg):
 
-
-
-# importing time module
-import time
-
-
-def message(string):
-
-	for i in string:
+	for i in range(0, len(msg)):
 	
 		# printing each character of the message
-		print(i, end="")
+		cprint(msg[i], end="")
 		
 		# adding time delay of half second
-		time.sleep(0.03)
+		time.sleep(0.04)
 
 
 # main function
-if __name__ == '__main__':
-	msg = "You wake up in a room. \nThere are no windows,  you don't know your name"
-	
-	# calling the function for printing the
-	# characters with delay
-	message(msg)
+
+def start():
+	stats()
+	time.sleep(1)
+	message("\nWelcome to the Game!")
+
+start()
