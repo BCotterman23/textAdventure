01cf9d3f05c653030c4a46d997f1bb7a98f4e7fb
BCotterman23 <817363@mystma.org>
2023-04-13 21:39:35 -0500

    update test.py

--- test.py
+++ test.py
@@ -1,23 +1,5 @@
 print(ord("%"))
 print(chr(37))
-import random
-def test():
-    armor = 3
-    return armor
-x = test()
-print("{}".format(x))
-#print("The result is{}".format(test))
-
-
-def testTwo():
-    print("hello\"")
-
-y = testTwo
-y()
-
-num = int(random.randrange(1,11))
-print(num)
-
 #Fore: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 #Back: BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET.
 #Style: DIM, NORMAL, BRIGHT, RESET_ALL
